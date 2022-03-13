7ae8f3ce217ffbf84dc2582dcecf03d36efedf74
Nafil Arzzam <arzzam19@gmail.com>
2022-03-13 21:39:10 +0530

    Finished

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -7,60 +7,60 @@ const Board = (props) => {
       <div>
         <Square
           value={props.squares[0]}
-          onclick={() => {
-            props.onclick(0);
+          onClick={() => {
+            props.onClick(0);
           }}
         />
         <Square
           value={props.squares[1]}
-          onclick={() => {
-            props.onclick(1);
+          onClick={() => {
+            props.onClick(1);
           }}
         />
         <Square
           value={props.squares[2]}
-          onclick={() => {
-            props.onclick(2);
+          onClick={() => {
+            props.onClick(2);
           }}
         />
       </div>
       <div>
         <Square
           value={props.squares[3]}
-          onclick={() => {
-            props.onclick(3);
+          onClick={() => {
+            props.onClick(3);
           }}
         />
         <Square
           value={props.squares[4]}
-          onclick={() => {
-            props.onclick(4);
+          onClick={() => {
+            props.onClick(4);
           }}
         />
         <Square
           value={props.squares[5]}
-          onclick={() => {
-            props.onclick(5);
+          onClick={() => {
+            props.onClick(5);
           }}
         />
       </div>
       <div>
         <Square
           value={props.squares[6]}
-          onclick={() => {
-            props.onclick(6);
+          onClick={() => {
+            props.onClick(6);
           }}
         />
         <Square
           value={props.squares[7]}
-          onclick={() => {
-            props.onclick(7);
+          onClick={() => {
+            props.onClick(7);
           }}
         />
         <Square
           value={props.squares[8]}
-          onclick={() => {
-            props.onclick(8);
+          onClick={() => {
+            props.onClick(8);
           }}
         />
       </div>
